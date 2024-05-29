cc164a49760f65186a771df84edd29c6418e7b38
Tugrul Karakaya <tugrul.karakaya@gmail.com>
2024-05-29 23:42:24 +0100

    First executable version of app.

---
+++ src/main/java/co/uk/zing/ledger/account/AccountService.java
@@ -0,0 +1,21 @@
+package co.uk.zing.ledger.account;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AccountService {
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account createAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    public Optional<Account> getAccountById(Long id) {
+        return accountRepository.findById(id);
+    }
+
+}
